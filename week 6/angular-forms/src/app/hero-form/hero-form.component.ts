41cb34e1b24307bce7543cd964a45eed98aa2cf1
Joey <joeyr699@gmail.com>
2019-10-15 11:47:31 -0400

    lab 6 completion

---
+++ week 6/angular-forms/src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+  powers = ['Really Smart', 'Super Flexable', 'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  OnSubmit(){
+    this.submitted = true;
+  }
+
+  getDiagnostic(){
+    return JSON.stringify(this.model);
+  }
+
+  newHero(){
+    this.model = new Hero(42, '', '' );
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
